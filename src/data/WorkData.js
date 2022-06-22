7f5c6a085c2dfff96b1de678cada49c364486933
shubh-iris <shubhamyadav.nitj@gmail.com>
2022-06-22 23:02:01 +0530

    initial commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,55 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "Dice Game",
+    description:
+      "It is build using core concepts of DOM manipulations just using HTML,JavaScript and CSS.",
+    tags: ["JavaScript", "CSS", "styledComponents", "HTML"],
+
+    demo: "https://github.com/shubh-iris/Projects/tree/main/Game",
+    github: "https://github.com/shubh-iris/Projects/tree/main/Game",
+  },
+  {
+    id: 2,
+    name: "Guess game",
+    description:
+      "A single guess the number game , implemented using JavaScript, CSS, HTML",
+    tags: ["JavaScript", "CSS", "DomManipulation"],
+
+    demo: "https://github.com/shubh-iris/Projects/tree/main/Guess",
+    github: "https://github.com/shubh-iris/Projects/tree/main/Guess",
+  },
+  {
+    id: 3,
+    name: "Vyam",
+    description:
+      "Vyam app is used keep the record of workouts. It marks location on map where workout is done. Used Leaflet library and geolocation api ",
+    tags: ["react", "api", "ejs", "javascript", "Leaflet ", "Geolocation API"],
+
+    demo: "https://github.com/shubh-iris/DevProjects/tree/main/Vyam",
+    github: "https://github.com/shubh-iris/DevProjects/tree/main/Vyam",
+  },
+  {
+    id: 4,
+    name: "Music Master",
+    description:
+      "Music master is the search based application which search artist and give result of top 10 artist songs. Used Spotify API",
+    tags: ["react", "styledComponents", "react-router", "SpofiyAPI"],
+
+    demo: "https://github.com/shubh-iris/DevProjects/tree/main/music-master",
+    github: "https://github.com/shubh-iris/DevProjects/tree/main/music-master",
+  },
+
+  {
+    id: 5,
+    name: "Url Shortner App",
+    description: "A web app used to generated smaller version of Urls  ",
+    tags: ["NodeJs", "ExpressJs", "MongoDB", "Parcel"],
+
+    demo: "https://github.com/shubh-iris/DevProjects/tree/main/url-shortener-master",
+    github:
+      "https://github.com/shubh-iris/DevProjects/tree/main/url-shortener-master",
+  },
+];
